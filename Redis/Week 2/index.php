3375239d18b8a80ace8fd7b44856772613a8c5a6
leonardo <nickholasleonardo@gmail.com>
2024-05-19 16:31:33 +0700

    feat: finalization

--- Redis/Week 2/index.php
+++ Redis/Week 2/index.php
@@ -80,7 +80,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Temperature Data Viewer</title>
+    <title>Global Land Temperature</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
